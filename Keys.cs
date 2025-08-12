326546d99bc9d5a6938a02ce05d68fd090b8b25d
lucasdev190 <lucasgtapc1@gmail.com>
2025-08-12 19:03:08 -0300

    Add files via upload

---
+++ Keys.cs
@@ -0,0 +1,12 @@
+namespace LicenseData
+{
+    public static class Keys
+    {
+        public static readonly string[] All =
+        {
+            "ABCDE-12345",
+            "FGHIJ-67890",
+            "KLMNO-24680"
+        };
+    }
+}
